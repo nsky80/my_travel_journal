2e22ec61c2c8d1c24e97a3f4edd2850b26af80c3
Satish NSKY80 <satishkumary80@gmail.com>
2022-01-21 16:49:42 +0530

    navbar modified for responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,12 +2,15 @@
 export default function Navbar() {
     return (
         <nav>
-            {/* <header className="App-header"> */}
-            <img src="./images/nav-icon.png" className="nav--logo" alt="logo" />
-
-            {/* </header> */}
-            <h3 className="nav--text">My Travel Journal</h3>
-
+            <div className="nav--centered">
+                <a href="/">
+                <ul><li><img src="./images/nav-icon.png" className="nav--logo" alt="logo" /></li>
+                    <li>
+                        <h3 className="nav--text">My Travel Journal.</h3>
+                    </li>
+                </ul>
+                </a>
+            </div>
         </nav>
     )
 }
