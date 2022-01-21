4507d2f6e81984f1c2939c39798375ccea95ab84
Satish NSKY80 <satishkumary80@gmail.com>
2022-01-21 23:53:18 +0530

    Card stylized

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,20 +1,21 @@
 
 export default function Card(props) {
-    console.log(props)
     return (
         <div className="card">
-            
+
             <div className="row">
                 <div className="column-1">
-                    <img src={props.imageUrl} alt="destination" className="card--image"/>
+                    <img src={props.imageUrl} alt="destination" className="card--image" />
                 </div>
-                
+
                 <div className="column-2">
-                    {props.location}
-                    <a href={props.googleMapsUrl}>View on Google Maps</a>
-                    {props.title}
-                    {props.startDate}-{props.endDate}
-                    {props.description}
+                    <div className="card--info--section">
+                        <span class="material-icons card--location-icon">place</span>
+                        <span className="card--location">{props.location}&nbsp; &nbsp;</span>
+                        <a href={props.googleMapsUrl} className="card--google-maps--link">View on Google Maps</a>                        <h1 className="card--title">{props.title}</h1>
+                        <span className="card--date">{props.startDate}-{props.endDate}</span>
+                        <p className="card--description">{props.description}</p>
+                    </div>
                 </div>
             </div>
             <hr />
--- src/data.js
+++ src/data.js
@@ -9,13 +9,13 @@ export default [
         imageUrl: "./images/mount-fuji.jfif"
     },
     {
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "./images/mount-fuji.jfif"
+        title: "Mount Everest",
+        location: "Nepal",
+        googleMapsUrl: "https://goo.gl/maps/uRTpGF5jGHQGbwed6",
+        startDate: "12 Jan, 2022",
+        endDate: "24 Jan, 2022",
+        description: "Mount Everest is Earth's highest mountain above sea level, located in the Mahalangur Himal sub-range of the Himalayas.",
+        imageUrl: "./images/mount-everest.jpg"
     },
     {
         title: "Mount Fuji",
